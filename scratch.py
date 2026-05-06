28c020bd27b927960979d862c0e7fbc4b08621e7
Web Developer <jorge.zuluaga@udea.edu.co>
2026-05-06 08:30:11 -0500

    refactor: remove obsolete DDC classification scripts and data files

--- scratch.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open('update/bookbuddy.htm', 'r', encoding='utf-8') as f:
-    html_content = f.read()
-
-soup = BeautifulSoup(html_content, 'html.parser')
-titles = soup.find_all('td', class_='title')
-container = titles[0].find_parent('table')
-print(container.prettify()[:1000])
